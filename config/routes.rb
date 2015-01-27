912d36bdbcdbc29047407af751fc815217107e3f
Gonzalo Sanchez <gonzalo.sanchez.d@gmail.com>
2015-01-27 12:03:10 -0300

    product#index is the new root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
   resources :productos
-
+  root "productos#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
