f6c2ef229f629c9fe184fcdefbe4ca7bbd69fbdc
Gonzalo Sanchez <gonzalo.sanchez.d@gmail.com>
2015-01-27 10:47:00 -0300

    authenticate product controller

--- app/controllers/productos_controller.rb
+++ app/controllers/productos_controller.rb
@@ -1,6 +1,7 @@
 class ProductosController < ApplicationController
   before_action :set_producto, only: [:show, :edit, :update, :destroy]
-
+  before_action :authenticate_user!
+  
   # GET /productos
   # GET /productos.json
   def index
