27119a30db71f1bb1f91500061f9ee29bce1e57b
Gonzalo Sanchez <gonzalo.sanchez.d@gmail.com>
2015-01-27 17:40:59 -0300

    adding relations n to n between users and products(productos)

--- app/models/pedido.rb
+++ app/models/pedido.rb
@@ -1,2 +1,5 @@
 class Pedido < ActiveRecord::Base
+	belongs_to :productos
+	belongs_to :user
+
 end
--- app/models/producto.rb
+++ app/models/producto.rb
@@ -1,2 +1,5 @@
 class Producto < ActiveRecord::Base
+	has_many :pedidos
+	has_many :users, through: :pedidos
+
 end
