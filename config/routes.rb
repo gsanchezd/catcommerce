48b1cf206d4bde2d9d363e6cdb918428ffdef85e
Gonzalo Sanchez <gonzalo.sanchez.d@gmail.com>
2015-01-27 17:01:05 -0300

    devise for user fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :productos
   root "productos#index"
+  devise_for :user
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
